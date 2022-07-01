c270c1036df5a7fd859c7306bdd83642cced800b
TanyaAng <t.j.angelova@gmail.com>
2022-07-01 10:33:41 +0300

    Exercises

--- 03_Classes_and_Objects_Lab/04_glass.py
+++ 03_Classes_and_Objects_Lab/04_glass.py
@@ -1,21 +1,28 @@
 class Glass:
+    initial_content = 0
     capacity = 250
 
     def __init__(self):
-        self.content = 0
+        self.content = self.initial_content
 
-    def fill(self, value):
-        if Glass.capacity-self.content>=value:
-            self.content+=value
-            return f"Glass filled with {value} ml"
-        return f"Cannot add {value} ml"
+    def fill(self, ml):
+        space_left = self.calculate_space_left()
+        if space_left < ml:
+            return f"Cannot add {ml} ml"
+        self.content += ml
+        return f"Glass filled with {ml} ml"
+
 
     def empty(self):
-        self.content=0
+        self.content = 0
         return f"Glass is now empty"
 
     def info(self):
-        return f"{Glass.capacity-self.content} ml left"
+        space_left = self.calculate_space_left()
+        return f"{space_left} ml left"
+
+    def calculate_space_left(self):
+        return self.capacity - self.content
 
 
 glass = Glass()
