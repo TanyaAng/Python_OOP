211f29809cb73af8c5efeab025aac1de9a5f7449
TanyaAng <t.j.angelova@gmail.com>
2022-07-16 15:45:33 +0300

    Exercises

--- 12_Polymorphism_and_Abstraction_Exercise/03_account.py
+++ 12_Polymorphism_and_Abstraction_Exercise/03_account.py
@@ -37,15 +37,15 @@ class Account:
     def __getitem__(self, item):
         return self._transactions[item]
 
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.__index < len(self._transactions):
-            item = self.__index
-            self.__index += 1
-            return self._transactions[item]
-        raise StopIteration
+    # def __iter__(self):
+    #     return self
+    #
+    # def __next__(self):
+    #     if self.__index < len(self._transactions):
+    #         item = self.__index
+    #         self.__index += 1
+    #         return self._transactions[item]
+    #     raise StopIteration
 
     def __eq__(self, other):
         return self.balance == other.balance
@@ -64,29 +64,29 @@ class Account:
         return new_account
 
 
-# acc = Account('bob', 10)
-# acc2 = Account('john')
-# print(acc)
-# print(repr(acc))
-# acc.add_transaction(20)
-# acc.add_transaction(-20)
-# acc.add_transaction(30)
-# print(acc.balance)
-# print(len(acc))
-# for transaction in acc:
-#     print(transaction)
-# print(acc[1])
-# print(list(reversed(acc)))
-# acc2.add_transaction(10)
-# acc2.add_transaction(60)
-# print(acc > acc2)
-# print(acc >= acc2)
-# print(acc < acc2)
-# print(acc <= acc2)
-# print(acc == acc2)
-# print(acc != acc2)
-# acc3 = acc + acc2
-# print(acc3)
-# print(acc3._transactions)
+acc = Account('bob', 10)
+acc2 = Account('john')
+print(acc)
+print(repr(acc))
+acc.add_transaction(20)
+acc.add_transaction(-20)
+acc.add_transaction(30)
+print(acc.balance)
+print(len(acc))
+for transaction in acc:
+    print(transaction)
+print(acc[1])
+print(list(reversed(acc)))
+acc2.add_transaction(10)
+acc2.add_transaction(60)
+print(acc > acc2)
+print(acc >= acc2)
+print(acc < acc2)
+print(acc <= acc2)
+print(acc == acc2)
+print(acc != acc2)
+acc3 = acc + acc2
+print(acc3)
+print(acc3._transactions)
 
 
