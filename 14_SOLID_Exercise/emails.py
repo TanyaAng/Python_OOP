218f12dcfa8d3310b824b23d9251dd11e1b70efd
TanyaAng <t.j.angelova@gmail.com>
2022-07-23 18:40:24 +0300

    Exercises

--- 14_SOLID_Exercise/emails.py
+++ 14_SOLID_Exercise/emails.py
@@ -1,8 +1,9 @@
-from abc import ABCMeta, abstractmethod
+# Single Responsibility
 
-class IEmail(object):
-    __metaclass__ = ABCMeta
+from abc import ABC, abstractmethod
 
+
+class IEmail(ABC):
     @abstractmethod
     def set_sender(self, sender):
         pass
@@ -15,11 +16,27 @@ class IEmail(object):
     def set_content(self, content):
         pass
 
-class Email(IEmail):
 
-    def __init__(self, protocol, content_type):
+class IContent(ABC):
+    def __init__(self, text):
+        self.text = text
+
+    @abstractmethod
+    def format(self):
+        pass
+
+
+class MyContent(IContent):
+    def __init__(self, text):
+        super().__init__(text)
+
+    def format(self):
+        return '\n'.join(['<myML>', self.text, '</myML>'])
+
+
+class Email(IEmail):
+    def __init__(self, protocol):
         self.protocol = protocol
-        self.content_type = content_type
         self.__sender = None
         self.__receiver = None
         self.__content = None
@@ -37,21 +54,16 @@ class Email(IEmail):
             self.__receiver = receiver
 
     def set_content(self, content):
-        if self.content_type == 'MyML':
-            self.__content = '\n'.join(['<myML>', content, '</myML>'])
-        else:
-            self.__content = content
+        self.__content = content.format()
 
     def __repr__(self):
-
         template = "Sender: {sender}\nReceiver: {receiver}\nContent:\n{content}"
+        return template.format(sender=self.__sender, receiver=self.__receiver, content=self.__content)
 
-        return template.format(sender = self.__sender, receiver = self.__receiver, content = self.__content)
 
-
-email = Email('IM', 'MyML')
+email = Email('IM')
 email.set_sender('qmal')
 email.set_receiver('james')
-email.set_content('Hello, there!')
+content = MyContent('Hello, there!')
+email.set_content(content)
 print(email)
-
