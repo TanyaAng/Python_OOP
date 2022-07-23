d99253d7deeec6a52f40d7de3253669eede4e886
TanyaAng <t.j.angelova@gmail.com>
2022-07-23 21:59:10 +0300

    Exercises

--- 16_Iterators_and_Generators_exercise/08_prime_numbers.py
+++ 16_Iterators_and_Generators_exercise/08_prime_numbers.py
@@ -1,6 +1,9 @@
+from math import sqrt
+
+
 def is_prime(number):
     if number > 1:
-        for i in range(2, number):
+        for i in range(2, int(sqrt(number)) + 1):
             if number % i == 0:
                 return False
         return True
