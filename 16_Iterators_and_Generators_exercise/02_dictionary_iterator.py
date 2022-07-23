1edfb4bad8066d02cccb65f74311f908ef6a15cd
TanyaAng <t.j.angelova@gmail.com>
2022-07-23 20:34:52 +0300

    Exercises

--- 16_Iterators_and_Generators_exercise/02_dictionary_iterator.py
+++ 16_Iterators_and_Generators_exercise/02_dictionary_iterator.py
@@ -1,20 +1,17 @@
 class dictionary_iter:
     def __init__(self, dictionary):
-        self.dictionary_items = dictionary
-        self.lenght = len(self.dictionary_items)
-        self.keys = list(self.dictionary_items.keys())
-        self.current = 0
+        self.items = list(dictionary.items())
+        self.idx = 0
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.current >= self.lenght:
+        if self.idx >= len(self.items):
             raise StopIteration
-        key_to_return = self.keys[self.current]
-        value_to_return = self.dictionary_items[key_to_return]
-        self.current+=1
-        return (key_to_return, value_to_return)
+        result = self.items[self.idx]
+        self.idx += 1
+        return result
 
 
 result = dictionary_iter({"name": "Peter", "age": 24})
