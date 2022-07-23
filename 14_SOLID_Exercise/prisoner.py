f74416143fc4fff4724236bf4ac208ae53e999f2
TanyaAng <t.j.angelova@gmail.com>
2022-07-23 18:16:35 +0300

    Exercises

--- 14_SOLID_Exercise/prisoner.py
+++ 14_SOLID_Exercise/prisoner.py
@@ -1,9 +1,14 @@
 import copy
 
-class Person:
 
+class Person:
     def __init__(self, position):
         self.position = position
+
+
+class FreePerson(Person):
+    def __init__(self, position):
+        super().__init__(position)
 
     def walk_north(self, dist):
         self.position[1] += dist
@@ -16,9 +21,8 @@ class Prisoner(Person):
     PRISON_LOCATION = [3, 3]
 
     def __init__(self):
-        super(Prisoner, self).__init__(copy.copy(self.PRISON_LOCATION))
+        super().__init__(copy.copy(self.PRISON_LOCATION))
         self.is_free = False
-
 
 
 prisoner = Prisoner()
