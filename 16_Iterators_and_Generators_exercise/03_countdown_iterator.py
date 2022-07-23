725c7867e92de7ae9a5f853d92633fe670dede0f
TanyaAng <t.j.angelova@gmail.com>
2022-07-23 21:31:01 +0300

    Exercises

--- 16_Iterators_and_Generators_exercise/03_countdown_iterator.py
+++ 16_Iterators_and_Generators_exercise/03_countdown_iterator.py
@@ -1,16 +1,16 @@
 class countdown_iterator:
     def __init__(self, count):
         self.count = count
-        self.start = self.count
+
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.start < 0:
+        if self.count < 0:
             raise StopIteration
-        value = self.start
-        self.start -= 1
+        value = self.count
+        self.count -= 1
         return value
 
 iterator = countdown_iterator(10)
