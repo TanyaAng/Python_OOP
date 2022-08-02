58e3b6fb79f8bb8d5f6567d5b000b26d9d0c679c
TanyaAng <t.j.angelova@gmail.com>
2022-08-02 21:07:20 +0300

    Exercises

--- Exam_Preparation/10_apr_2022/project/supply/drink.py
+++ Exam_Preparation/10_apr_2022/project/supply/drink.py
@@ -4,5 +4,5 @@ from project.supply.supply import Supply
 class Drink(Supply):
     INITIAL_ENERGY = 15
 
-    def __init__(self, name):
-        super().__init__(name, self.INITIAL_ENERGY)
+    def __init__(self, name, energy=INITIAL_ENERGY):
+        super().__init__(name, energy)
